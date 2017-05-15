a9a52a3f18c171266513d2f404577e0668dd4848
Matt Zhang <set.stun@gmail.com>
2017-05-15 07:11:26 +0200

    Updated skimming script to be compatible with lxplus environment

--- h5pp.py
+++ h5pp.py
@@ -7,7 +7,7 @@ def deleteRows(oldFile, dsetName, badRows, newFile):
     badRows.sort() # sort in increasing order
 
     oldDset = oldFile[dsetName]
-    if oldDset.ndim == 1: # force 1D array to be 2D, with a second dimension of 1
+    if len(oldDset.shape) == 1: # force 1D array to be 2D, with a second dimension of 1
         oldDset = np.reshape(oldDset, (-1, 1)) # numpy stacking only works with "2D arrays"
 
     newShape = list(oldDset.shape)
@@ -18,28 +18,14 @@ def deleteRows(oldFile, dsetName, badRows, newFile):
     # Filling new dataset #
     #######################
 
-    # newDsetCmd = "newDset = np.vstack(["
-    # if (len(badRows) is not 1) and (badRows[0] is not 0): newDsetCmd += "oldDset[0:"+str(badRows[0])+"], "
-
-    # for index, rowN in enumerate(badRows):
-
-        # newDsetCmd += "oldDset["+str(rowN+1)+":"
-
-        # if index+1 != len(badRows):
-            # newDsetCmd += str(badRows[index+1])+"], "
-        # else:
-            # newDsetCmd += "]])"
-
-    # exec(newDsetCmd)
-
     for index, rowN in enumerate(badRows):
 
         if (index == 0):
             if (rowN is not 0): # if the first bad index is not 0
                 newDset[0:rowN] = oldDset[0:rowN] # add the slice before the first index 
 
-        else:
+        elif (badRows[index-1]+1 != rowN): # ignore zero-length slices
             newDset[badRows[index-1]+1-index:rowN-index] = oldDset[badRows[index-1]+1:rowN] # e.g. if indices 5 and 12 are bad, add the slice [6:12]
 
-        if (index+1 == len(badRows)):
+        if (index+1 == len(badRows) and rowN+1 != len(oldDset)):
             newDset[rowN-index:] = oldDset[rowN+1:] # fill the remainder
--- skimH5.py
+++ skimH5.py
@@ -2,11 +2,16 @@ import h5py
 import h5pp
 import numpy as np
 
-oldFilePath = "H5Files/"
-newFilePath = "SkimmedH5Files/"
-filesToSkim = ["gamma_60_GeV.h5"]
+oldFilePath = "H5Files/v1/Unskimmed/"
+newFilePath = "H5Files/v1/Skimmed/"
+
+for fileN in range(1, 11):
+    filesToSkim.append("gamma_60_GeV_"+str(fileN)+".h5")
+    filesToSkim.append("pi0_60_GeV_"+str(fileN)+".h5")
 
 for fileName in filesToSkim:
+
+    print "Skimming", fileName
 
     oldFile = h5py.File(oldFilePath + fileName)
     newFile = h5py.File(newFilePath + fileName, "w")
