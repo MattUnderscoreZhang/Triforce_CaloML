3c72cd2a1f66ad4fe3e7edf233d0df6121c23ce5
Matt Zhang <set.stun@gmail.com>
2017-05-24 10:41:44 -0500

    Set gun direction to be perpendicular (z is axis of particle, perpendicular to calorimeter surface)

--- Generating/runDdsim.sh
+++ Generating/runDdsim.sh
@@ -8,7 +8,7 @@ for energy in `seq 50 10 100`
 do
     for fileN in {1..10}
     do
-        ddsim --compactFile /cvmfs/clicdp.cern.ch/iLCSoft/builds/2016-11-22/x86_64-slc6-gcc48-opt/lcgeo/HEAD/CLIC/compact/CLIC_o3_v07/CLIC_o3_v07.xml -O ROOTFiles/pi0_${energy}_GeV_${fileN}.root -N 1000 --enableGun --gun.particle pi0 --gun.energy $energy*GeV --gun.distribution uniform
-        ddsim --compactFile /cvmfs/clicdp.cern.ch/iLCSoft/builds/2016-11-22/x86_64-slc6-gcc48-opt/lcgeo/HEAD/CLIC/compact/CLIC_o3_v07/CLIC_o3_v07.xml -O ROOTFiles/gamma_${energy}_GeV_${fileN}.root -N 1000 --enableGun --gun.particle gamma --gun.energy $energy*GeV --gun.distribution uniform
+        ddsim --compactFile /cvmfs/clicdp.cern.ch/iLCSoft/builds/2016-11-22/x86_64-slc6-gcc48-opt/lcgeo/HEAD/CLIC/compact/CLIC_o3_v07/CLIC_o3_v07.xml -O ROOTFiles/pi0_${energy}_GeV_${fileN}.root -N 1000 --enableGun --gun.particle pi0 --gun.energy $energy*GeV --gun.distribution uniform --gun.direction 1,0,0
+        ddsim --compactFile /cvmfs/clicdp.cern.ch/iLCSoft/builds/2016-11-22/x86_64-slc6-gcc48-opt/lcgeo/HEAD/CLIC/compact/CLIC_o3_v07/CLIC_o3_v07.xml -O ROOTFiles/gamma_${energy}_GeV_${fileN}.root -N 1000 --enableGun --gun.particle gamma --gun.energy $energy*GeV --gun.distribution uniform --gun.direction 1,0,0
     done
 done
