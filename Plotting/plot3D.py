48653c049f3b8f57c77b4cd1ee9a6b95f1a929d7
Matt Zhang <set.stun@gmail.com>
2017-06-01 20:25:49 -0500

    Added save functionality to plots, and also average plotting

--- Plotting/plot3D.py
+++ Plotting/plot3D.py
@@ -2,11 +2,16 @@ import matplotlib.pyplot as plt
 import matplotlib.colors
 import matplotlib.cm as cmx
 from mpl_toolkits.mplot3d import Axes3D
+import os
 
 import h5py
 import numpy as np
 
-def scatter3d(x,y,z, values, colorsMap='jet'):
+#####################
+# PLOTTING FUNCTION #
+#####################
+
+def scatter3d(x, y, z, values, colorsMap='jet', saveName='Plots/Test.pdf'):
 
     cm = plt.get_cmap(colorsMap) # set up color map
     cNorm = matplotlib.colors.Normalize(vmin=min(values), vmax=max(values)) # normalize to min and max of data
@@ -23,21 +28,40 @@ def scatter3d(x,y,z, values, colorsMap='jet'):
 
     scalarMap.set_array(values)
     fig.colorbar(scalarMap) # plot color bar
-    plt.show()
 
-data = h5py.File("../AllFiles/SkimmedH5Files/pi0_60_GeV_1.h5")
+    #plt.show()
+    plt.savefig(saveName, bbox_inches="tight")
+
+#########################
+# CHOOSE EVENTS TO PLOT #
+#########################
+
+data = h5py.File("../AllFiles/H5Files/v1/Skimmed/Pi0/pi0_60_GeV_1.h5")
 events = [0, 1, 2, 3, 4]
 
+saveDirectory = "Plots/"
+if not os.path.exists(saveDirectory): os.makedirs(saveDirectory)
+
+# plot each event
 for eventN in events:
 
     ECAL = data['ECAL']
     x, y, z = np.nonzero(ECAL[eventN]) # first event
     E = ECAL[eventN][np.nonzero(ECAL[eventN])]
-    print x.shape, y.shape, z.shape, E.shape
-    scatter3d(x,y,z, E)
+    scatter3d(x, y, z, E, saveName=saveDirectory+"ECAL_event"+str(eventN)+".pdf")
 
     HCAL = data['HCAL']
     x, y, z = np.nonzero(HCAL[eventN]) # first event
     E = HCAL[eventN][np.nonzero(HCAL[eventN])]
-    print x.shape, y.shape, z.shape, E.shape
-    scatter3d(x,y,z, E)
+    scatter3d(x, y , z, E, saveName=saveDirectory+"HCAL_event"+str(eventN)+".pdf")
+
+# plot averages
+ECAL_average = np.average(data['ECAL'], axis=0)
+x, y, z = np.nonzero(ECAL_average) # first event
+E = ECAL_average[np.nonzero(ECAL_average)]
+scatter3d(x, y, z, E, saveName=saveDirectory+"ECAL_average.pdf")
+
+HCAL_average = np.average(data['HCAL'], axis=0)
+x, y, z = np.nonzero(HCAL_average) # first event
+E = HCAL_average[np.nonzero(HCAL_average)]
+scatter3d(x, y , z, E, saveName=saveDirectory+"HCAL_average.pdf")
