bc0ba1520f0c442963cb279fcfdfd962ae2738a3
Matt Zhang <set.stun@gmail.com>
2017-06-05 19:20:52 -0500

    Fixed problem with skimming when there were no bad rows

--- Skimming/h5pp.py
+++ Skimming/h5pp.py
@@ -18,14 +18,18 @@ def deleteRows(oldFile, dsetName, badRows, newFile):
     # Filling new dataset #
     #######################
 
-    for index, rowN in enumerate(badRows):
+    if len(badRows == 0):
+        newDset = oldDset
 
-        if (index == 0):
-            if (rowN is not 0): # if the first bad index is not 0
-                newDset[0:rowN] = oldDset[0:rowN] # add the slice before the first index 
+    else:
+        for index, rowN in enumerate(badRows):
 
-        elif (badRows[index-1]+1 != rowN): # ignore zero-length slices
-            newDset[badRows[index-1]+1-index:rowN-index] = oldDset[badRows[index-1]+1:rowN] # e.g. if indices 5 and 12 are bad, add the slice [6:12]
+            if (index == 0):
+                if (rowN is not 0): # if the first bad index is not 0
+                    newDset[0:rowN] = oldDset[0:rowN] # add the slice before the first index 
 
-        if (index+1 == len(badRows) and rowN+1 != len(oldDset)):
-            newDset[rowN-index:] = oldDset[rowN+1:] # fill the remainder
+            elif (badRows[index-1]+1 != rowN): # ignore zero-length slices
+                newDset[badRows[index-1]+1-index:rowN-index] = oldDset[badRows[index-1]+1:rowN] # e.g. if indices 5 and 12 are bad, add the slice [6:12]
+
+            if (index+1 == len(badRows) and rowN+1 != len(oldDset)):
+                newDset[rowN-index:] = oldDset[rowN+1:] # fill the remainder
