c94ad996222931e049c24e6df404433a2cc1c67a
Matt Zhang <set.stun@gmail.com>
2017-09-08 07:47:34 +0200

    Forgot to commit a file

--- Converting/Convert_to_h5.py
+++ Converting/Convert_to_h5.py
@@ -3,12 +3,12 @@
 # Maurizio Pierni (April 2017)
 # Matt Zhang (May 2017)
 
+from __future__ import division
 import numpy as np
 import scipy.stats
 import sys
 import ast
 import h5py
-from __future__ import division
 
 ##################################
 # Functions for finding centroid #
@@ -28,14 +28,14 @@ def findEventMidpoint(event):
 # Jet functions for calculating nsubjettiness #
 ###############################################
 
-# Find dR between two postiions (eta, phi)
+# Find dR between two positions (eta, phi)
 def dR(position1, position2):
-    return np.sqrt(np.pow(position1[0] - position2[0], 2), np.pow(position1[1] - position2[1], 2))
+    return np.sqrt(np.power(position1[0] - position2[0], 2) + np.power(position1[1] - position2[1], 2))
 
-# Calculate eta
+# Calculate eta (from Wikipedia)
 def eta(r, z):
-    theta = np.arctan(r/z)
-    return -np.log(np.tan(theta/2))
+    p = np.sqrt(r*r + z*z)
+    return 0.5 * np.log((p + z) / (p - z))
 
 # All particles with energy above a threshold - returns [particle1, particle2...], where particle = (eta, phi, pT)
 # eventVector is in the form (px, py, pz) for the calorimeter slice
@@ -65,36 +65,43 @@ def particlesAboveThreshold(caloData, threshold, eventVector):
     return zip(scaledEta, scaledPhi, E)
 
 # Find d_ij between two protojets
-def dij(v1, v2): # each vector is (eta, phi, E)
+def calc_dij(v1, v2): # each vector is (eta, phi, E)
     eta1, phi1, E1 = v1
     eta2, phi2, E2 = v2
-    return np.min(E1*E1, E2*E2) * (np.power(eta1-eta2, 2) + np.power(phi1-phi2, 2)) # letting R = 1
+    return min(E1*E1, E2*E2) * (np.power(eta1-eta2, 2) + np.power(phi1-phi2, 2)) # letting R = 1
 
 # based on exclusive-kT clustering algorithm from https://arxiv.org/pdf/hep-ph/9305266.pdf
 # however, I force protojet combination until exactly N jets are left, ignoring the possibility of beam jets
 # particles in the form [particle1, particle2...], where particle = (eta, phi, pT)
+# return [[1 jet], [2 jets]... [nJets]]
 # jets in form [jet1, jet2...], where jet = (eta, phi)
 def antiKtJets(particles, nJets):
 
-    protojets = particles[:]
+    setsOfBestJets = []
     nParticles = len(particles)
+    protojets = particles[:]
 
     # If there are not enough particles, just abort early and return an empty list
     if nParticles < nJets:
-        return []
+        return setsOfBestJets
 
     # Initial values of dij
     dij = np.zeros((nParticles, nParticles))
     for n1 in range(nParticles):
         for n2 in range(n1+1, nParticles):
-            dij[n1][n2] = dij(particles[n1], particles[n2])
+            dij[n1][n2] = calc_dij(particles[n1], particles[n2])
             dij[n2][n1] = dij[n1][n2] # Symmetric
     for n in range(nParticles):
         # dij[n][n] = np.power(particles[n][2], 2) # E_T squared
         dij[n][n] = 1000 # Ignore possibility of ending merge algorithm
 
+    # If the number of particles is exactly the same as the number of requested jets, start adding to return list
+    if len(protojets) <= nJets:
+        bestJets = [i[0:2] for i in protojets]
+        setsOfBestJets.insert(0, bestJets) # add to front of list
+
     # Update dij iteratively
-    while(len(protojets) > nJets):
+    while(len(protojets) > 1):
         lowestIndex = np.unravel_index(dij.argmin(), dij.shape)
         if (lowestIndex[0] != lowestIndex[1]): # Merge into one jet and give it the lower of two indices
             removedProtojetIndex = max(lowestIndex[0], lowestIndex[1])
@@ -110,14 +117,19 @@ def antiKtJets(particles, nJets):
             dij = np.delete(dij, removedProtojetIndex, axis=1)
             protojets[newProtojetIndex] = newProtojet
             for n in range(len(protojets)):
-                dij[n][newProtojetIndex] = dij(protojets[n], newProtojet)
+                dij[n][newProtojetIndex] = calc_dij(protojets[n], newProtojet)
                 dij[newProtojetIndex][n] = dij[n][newProtojetIndex] # Symmetric
-        # else:
+            # dij[newProtojetIndex][newProtojetIndex] = np.power(protojets[newProtojetIndex][2], 2) # E_T squared
+            dij[newProtojetIndex][newProtojetIndex] = 1000 # Ignore possibility of ending merge algorithm
+        else:
             # not considered
+            print "OH NO THIS SHOULDN'T BE HAPPENING!", dij[lowestIndex[0]][lowestIndex[1]]
+        if len(protojets) <= nJets:
+            bestJets = [i[0:2] for i in protojets]
+            setsOfBestJets.insert(0, bestJets) # add to front of list
 
-    # Return best jets
-    bestJets = [i[0:2] for i in protojets]
-    return bestJets
+    # Return sets of best jets
+    return setsOfBestJets
 
 ##################################################
 # Check if between bounds for calorimeter window #
@@ -325,15 +337,17 @@ def convertFile(inFile, outFile):
 
         # N-subjettiness algorithm, as described in this paper: https://arxiv.org/pdf/1011.2268.pdf.
         eventVector = (60, 0, 0) # change this to match event
-        threshold = np.mean(caloData)/20 # energy threshold for a calo hit
-        particles= particlesAboveThreshold(caloData, threshold, eventVector) # particles in the form (eta, phi, pT)
+        threshold = np.mean(ECALarray)/20 # energy threshold for a calo hit
+        particles = particlesAboveThreshold(ECALarray, threshold, eventVector) # particles in the form (eta, phi, pT)
         # After identifying N candidate subjets, we calculate tauN.
         # Ignoring characteristic jet radius, since I'm focused on exclusive jet clustering, and it's only a scalar for taus.
         tauN = []
-        for nJets in range(3):
-            # Jets reconstructed using anti-kT algorithm here: https://arxiv.org/pdf/hep-ph/9305266.pdf (use only ECAL data).
-            jets = antiKtJets(particles, nJets+1) 
-            if len(jets) > 0: # if jet-finding succeeded
+        # Return the sets of best 1, 2, and 3 jets
+        # Jets reconstructed using anti-kT algorithm here: https://arxiv.org/pdf/hep-ph/9305266.pdf (use only ECAL data).
+        nJets = 3
+        jetSets = antiKtJets(particles, nJets) 
+        if len(jetSets) > 0: # if jet-finding succeeded
+            for jets in jetSets:
                 tau = 0
                 d0 = 0 # normalization factor
                 for particle in particles:
@@ -341,17 +355,18 @@ def convertFile(inFile, outFile):
                     particlePosition = (particle[0], particle[1])
                     for jet in jets:
                         dRList.append(dR(particlePosition, jet))
-                    tau += particle[3] * min(dRList) # pT * min(dR)
-                    d0 += particle[3] # pT
+                    tau += particle[2] * min(dRList) # pT * min(dR)
+                    d0 += particle[2] # pT
                 tau = tau/d0
                 tauN.append(tau) 
-            else:
+        else:
+            for n in range(nJets):
                 tauN.append(1000) # put in a ridiculously large tau value 
-        myFeatures.add("N_Subjettiness/tau1", tau[0])
-        myFeatures.add("N_Subjettiness/tau2", tau[1])
-        myFeatures.add("N_Subjettiness/tau3", tau[2])
-        myFeatures.add("N_Subjettiness/tau2_over_tau1", tau[1]/tau[0])
-        myFeatures.add("N_Subjettiness/tau3_over_tau2", tau[2]/tau[1])
+        myFeatures.add("N_Subjettiness/tau1", tauN[0])
+        myFeatures.add("N_Subjettiness/tau2", tauN[1])
+        myFeatures.add("N_Subjettiness/tau3", tauN[2])
+        myFeatures.add("N_Subjettiness/tau2_over_tau1", tauN[1]/tauN[0])
+        myFeatures.add("N_Subjettiness/tau3_over_tau2", tauN[2]/tauN[1])
 
     # Save features to an h5 file
     f = h5py.File(outFile, "w")
