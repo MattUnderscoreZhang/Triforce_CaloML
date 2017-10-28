0f6b2568776301a39c21405a3c81c6763d9b2f52
wwei <stephe.wei@gmail.com>
2017-10-28 15:19:21 -0500

    fixed bugs in Normalize.py

--- Converting/python/Normalize.py
+++ Converting/python/Normalize.py
@@ -16,11 +16,16 @@ def normalizeFiles(inFile, outFile):
     for file_index in range(len(files)):
         f1 = h5py.File(files[file_index], 'r')
         for feature in f1.keys():
-            if feature in ['ECAL', 'HCAL'] pass
-            if file_index == 0:
-                totalFeatures[feature] = f1[feature]
-            else:
-                totalFeatures[feature] = np.concatentate(totalFeatures[feature], f1[feature])
+            if(feature!='ECAL' and feature !='HCAL' and feature!='pdgID' and feature!='conversion' and feature!='energy' and feature!='bestJets10' and feature!='bestJets11' and feature!='bestJets20' and feature!= 'bestJets20' and feature!='bestJets21' and feature!='tau1' and feature!='tau2' and feature!='tau3' and feature!='tau2_over_tau1' and feature!='tau3_over_tau2'):
+                if file_index == 0:
+                    features1=np.array(f1[feature][:])
+                    features1[features1==np.inf]=0
+                    features1[features1==-np.inf]=0
+                    features1=np.nan_to_num(features1)
+
+                    totalFeatures[feature] = features1
+                else:
+                    totalFeatures[feature] = np.concatenate((totalFeatures[feature], features1))
         f1.close()
 
     for feature in totalFeatures.keys():
@@ -29,11 +34,16 @@ def normalizeFiles(inFile, outFile):
 
     for file_index in range(len(files)):
         # Save features to an h5 file
-        f1 = h5py.File(outFile+os.path.basename(files[file_index]), "w")
-        for feature in f1.keys():
-            if feature in ['ECAL', 'HCAL'] pass
-            f1[feature] = (totalFeatures[feature] - means[feature])/stds[feature]
+        f1 = h5py.File(files[file_index], 'r')
+        f2 = h5py.File(outFile+os.path.basename(files[file_index]), "w")
+        for feature in totalFeatures:
+            features1=np.array(f1[feature][:])
+            features1[features1==np.inf]=0
+            features1[features1==-np.inf]=0
+            features1=np.nan_to_num(features1)
+            f2.create_dataset(feature,data= (features1 - means[feature])/stds[feature])
         f1.close()
+        f2.close()
     
 #################
 # Main function #
@@ -43,5 +53,4 @@ if __name__ == "__main__":
     import sys
     inFile = sys.argv[1]
     outFile = sys.argv[2]
-    pdgID = float(sys.argv[3])
-    convertFile(inFile, outFile, pdgID)
+    normalizeFiles(inFile, outFile)
