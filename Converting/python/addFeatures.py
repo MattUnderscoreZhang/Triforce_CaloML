53a2b9582e2a24cf6778d07484abe13ef08bb313
Matt Zhang <set.stun@gmail.com>
2017-11-01 23:26:23 -0500

    Changed moment scaling factors to make moments not diverge like crazy

--- Converting/python/addFeatures.py
+++ Converting/python/addFeatures.py
@@ -52,55 +52,59 @@ def convertFile(inFile, outFile):
     newFile.create_dataset("HCAL_Ratios/HCAL_ratioFirstLayerToSecondLayerE", data=HCAL_E_firstLayer/HCAL_E_secondLayer)
 
     # Used in moment calculation
-    EventMinRadius = 1.5
-    SliceCellRSize = 0.00636
-    SliceCellZSize = 0.0051
-    SliceCellPhiSize = 0.0051
+    # EventMinRadius = 1.5
+    # SliceCellRSize = 0.00636
+    # SliceCellZSize = 0.0051
+    # SliceCellPhiSize = 0.0051
+    EventMinRadius = 1 # chosen to make moments stay close to 1
+    SliceCellRSize = 0.25
+    SliceCellZSize = 0.25
+    SliceCellPhiSize = 0.25
 
     # NEED TO CONVERT TO FULL-ARRAY FORM
-    # # ECAL moments
-    # ECALprojX = np.sum(np.sum(ECALarray, axis=2), axis=1)
-    # ECALprojY = np.sum(np.sum(ECALarray, axis=2), axis=0)
-    # ECALprojZ = np.sum(np.sum(ECALarray, axis=0), axis=0) # z = direction into calo
-    # totalE = np.sum(ECALprojX)
-    # ECAL_sizeX, ECAL_sizeY, ECAL_sizeZ = ECALarray.shape 
-    # ECAL_midX = (ECAL_sizeX-1)/2
-    # ECAL_midY = (ECAL_sizeY-1)/2
-    # ECAL_midZ = (ECAL_sizeZ-1)/2
-    # for i in range(1, 6):
-        # moments = pow((np.range(ECAL_sizeX)-ECAL_midX)*EventMinRadius*SliceCellPhiSize, i+i)
-        # ECAL_momentX = np.sum(np.multiply(ECALprojX, moments))/totalE
-        # myFeatures.add("ECAL_Moments/ECALmomentX" + str(i+1), ECAL_momentX)
-    # for i in range(1, 6):
-        # moments = pow((np.range(ECAL_sizeY)-ECAL_midY)*SliceCellZSize, i+i)
-        # ECAL_momentY = np.sum(np.multiply(ECALprojY, moments))/totalE
-        # myFeatures.add("ECAL_Moments/ECALmomentY" + str(i+1), ECAL_momentY)
-    # for i in range(1, 6):
-        # moments = pow((np.range(ECAL_sizeZ)-ECAL_midZ)*SliceCellRSize, i+i)
-        # ECAL_momentZ = np.sum(np.multiply(ECALprojZ, moments))/totalE
-        # myFeatures.add("ECAL_Moments/ECALmomentZ" + str(i+1), ECAL_momentZ)
+    # ECAL moments
+    ECALprojX = np.sum(np.sum(ECAL, axis=3), axis=2)
+    ECALprojY = np.sum(np.sum(ECAL, axis=3), axis=1)
+    ECALprojZ = np.sum(np.sum(ECAL, axis=1), axis=1) # z = direction into calo
+    totalE = np.sum(ECALprojX, axis=1)
+    ECAL_sizeX, ECAL_sizeY, ECAL_sizeZ = ECAL[0].shape 
+    ECAL_midX = (ECAL_sizeX-1)/2
+    ECAL_midY = (ECAL_sizeY-1)/2
+    ECAL_midZ = (ECAL_sizeZ-1)/2
+    for i in range(1, 6):
+        moments = pow(abs(np.arange(ECAL_sizeX)-ECAL_midX)*EventMinRadius*SliceCellPhiSize, i+i)
+        ECAL_momentX = np.sum(np.multiply(ECALprojX, moments), axis=1)/totalE
+        newFile.create_dataset("ECAL_Moments/ECALmomentX" + str(i+1), data=ECAL_momentX)
+    for i in range(1, 6):
+        moments = pow(abs(np.arange(ECAL_sizeY)-ECAL_midY)*SliceCellZSize, i+i)
+        ECAL_momentY = np.sum(np.multiply(ECALprojY, moments), axis=1)/totalE
+        newFile.create_dataset("ECAL_Moments/ECALmomentY" + str(i+1), data=ECAL_momentY)
+    for i in range(1, 6):
+        moments = pow(abs(np.arange(ECAL_sizeZ)-ECAL_midZ)*SliceCellRSize, i+i)
+        ECAL_momentZ = np.sum(np.multiply(ECALprojZ, moments), axis=1)/totalE
+        newFile.create_dataset("ECAL_Moments/ECALmomentZ" + str(i+1), data=ECAL_momentZ)
 
-    # # HCAL moments
-    # HCALprojX = np.sum(np.sum(HCALarray, axis=2), axis=1)
-    # HCALprojY = np.sum(np.sum(HCALarray, axis=2), axis=0)
-    # HCALprojZ = np.sum(np.sum(HCALarray, axis=0), axis=0)
-    # totalE = np.sum(HCALprojX)
-    # HCAL_sizeX, HCAL_sizeY, HCAL_sizeZ = HCALarray.shape 
-    # HCAL_midX = (HCAL_sizeX-1)/2
-    # HCAL_midY = (HCAL_sizeY-1)/2
-    # HCAL_midZ = (HCAL_sizeZ-1)/2
-    # for i in range(1, 6):
-        # moments = pow((np.range(HCAL_sizeX)-HCAL_midX)*EventMinRadius*SliceCellPhiSize, i+i)
-        # HCAL_momentX = np.sum(np.multiply(HCALprojX, moments))/totalE
-        # myFeatures.add("HCAL_Moments/HCALmomentX" + str(i+1), HCAL_momentX)
-    # for i in range(1, 6):
-        # moments = pow((np.range(HCAL_sizeY)-HCAL_midY)*SliceCellZSize, i+i)
-        # HCAL_momentY = np.sum(np.multiply(HCALprojY, moments))/totalE
-        # myFeatures.add("HCAL_Moments/HCALmomentY" + str(i+1), HCAL_momentY)
-    # for i in range(1, 6):
-        # moments = pow((np.range(HCAL_sizeZ)-HCAL_midZ)*SliceCellRSize, i+i)
-        # HCAL_momentZ = np.sum(np.multiply(HCALprojZ, moments))/totalE
-        # myFeatures.add("HCAL_Moments/HCALmomentZ" + str(i+1), HCAL_momentZ)
+    # HCAL moments
+    HCALprojX = np.sum(np.sum(HCAL, axis=3), axis=2)
+    HCALprojY = np.sum(np.sum(HCAL, axis=3), axis=1)
+    HCALprojZ = np.sum(np.sum(HCAL, axis=1), axis=1)
+    totalE = np.sum(HCALprojX, axis=1)
+    HCAL_sizeX, HCAL_sizeY, HCAL_sizeZ = HCAL[0].shape 
+    HCAL_midX = (HCAL_sizeX-1)/2
+    HCAL_midY = (HCAL_sizeY-1)/2
+    HCAL_midZ = (HCAL_sizeZ-1)/2
+    for i in range(1, 6):
+        moments = pow(abs(np.arange(HCAL_sizeX)-HCAL_midX)*EventMinRadius*SliceCellPhiSize, i+i)
+        HCAL_momentX = np.sum(np.multiply(HCALprojX, moments), axis=1)/totalE
+        newFile.create_dataset("HCAL_Moments/HCALmomentX" + str(i+1), data=HCAL_momentX)
+    for i in range(1, 6):
+        moments = pow(abs(np.arange(HCAL_sizeY)-HCAL_midY)*SliceCellZSize, i+i)
+        HCAL_momentY = np.sum(np.multiply(HCALprojY, moments), axis=1)/totalE
+        newFile.create_dataset("HCAL_Moments/HCALmomentY" + str(i+1), data=HCAL_momentY)
+    for i in range(1, 6):
+        moments = pow(abs(np.arange(HCAL_sizeZ)-HCAL_midZ)*SliceCellRSize, i+i)
+        HCAL_momentZ = np.sum(np.multiply(HCALprojZ, moments), axis=1)/totalE
+        newFile.create_dataset("HCAL_Moments/HCALmomentZ" + str(i+1), data=HCAL_momentZ)
     
 #################
 # Main function #
