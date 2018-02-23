6e80737fd65a0c79f12361ddc6e44fbfaa6bdfd4
Matt Zhang <set.stun@gmail.com>
2018-02-22 23:11:40 -0600

    Further fixes to default options

--- Options/default_options.py
+++ Options/default_options.py
@@ -11,6 +11,8 @@ basePath = "/data/LCD/V3/Original/GammaPi0/"
 samplePath = [basePath + "Pi0/Pi0Escan_*.h5", basePath + "Gamma/GammaEscan_*.h5"]
 classPdgID = [111, 22] # absolute IDs corresponding to paths above
 eventsPerFile = 10000
+ECAL_size = (25, 25, 25)
+HCAL_size = (5, 5, 60)
 
 nworkers = 0 # number of workers in PyTorch DataLoader
 
@@ -30,23 +32,17 @@ OutPath = os.getcwd()+"/Output/"+sys.argv[1]+"/"
 # Choose tools #
 ################
 
-from Classification import GoogLeNet # NIPS_Classifier
+from Classification import NIPS_Classifier
 from Regression import NIPS_Regressor
 from GAN import NIPS_GAN
 from Analysis import Default_Analyzer
 
-# _learningRate = float(sys.argv[2])
-# _decayRate = float(sys.argv[3])
-# _dropoutProb = float(sys.argv[4])
-# _hiddenLayerNeurons = int(sys.argv[5])
-# _nHiddenLayers = int(sys.argv[6])
-_learningRate = 0.00001
-_decayRate = 0
-_dropoutProb = 0
-_hiddenLayerNeurons = None
-_nHiddenLayers = None
+_learningRate = float(sys.argv[2])
+_decayRate = float(sys.argv[3])
+_dropoutProb = float(sys.argv[4])
+_hiddenLayerNeurons = int(sys.argv[5])
+_nHiddenLayers = int(sys.argv[6])
 
-classifier = GoogLeNet.Classifier(_learningRate, _decayRate)
 classifier = NIPS_Classifier.Classifier(_hiddenLayerNeurons, _nHiddenLayers, _dropoutProb, _learningRate, _decayRate)
 # classifier = None # set a tool to None to ignore it
 regressor = NIPS_Regressor.Regressor(_learningRate, _decayRate)
