4549eda5b4f0c521546c317f484893e61fc22d30
justinliu412 <justinliu412@hotmail.com>
2018-03-29 22:33:35 -0500

    add loss plotting functions in Tools

--- Tools/Classification_Plotter.py
+++ Tools/Classification_Plotter.py
@@ -7,29 +7,107 @@ import numpy as np
 import os, sys, h5py
 import pdb
 
-def accuracy_vs_events(results, interval): 
+def accuracy_vs_batches(results, interval): 
     """
-    results = h5py.File('Output/Output008/results.h5')
+    results = 'Output/Output008/results.h5'
     Parameters
-    @results: the output file.h5 storing training and test results
+    @results: the paht of output file.h5 storing training and test results
     @interval: the interval between each sample point
     """
 
-    data = results['classifier_accuracy_history_train']
+    f = h5py.File(results, 'r')
+
+    data = f['classifier_accuracy_history_train']
+
+    if not os.path.exists(os.path.join(results.split('/')[0], 'Plots')): 
+        os.makedirs(os.path.join(results.split('/')[0], 'Plots'))
+    output_path = os.path.join(results.split('/')[0], 'Plots', 'accuracy_vs_batches.png')
 
     fig, ax = plt.subplots()
     sample_points = [(i + 1) * interval for i in range(len(data))]
     # plt.style.use('ggplot')
     ax.plot(sample_points, data, color='b')
     ax.grid()
-    ax.set_xlabel('Epoch of training')
+    ax.set_xlabel('Batches of training')
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Trends of Accuracy along Batches')
+
+    plt.savefig(output_path)
+
+def accuracy_vs_epochs(results): 
+    """
+    results = 'Output/Output008/results.h5'
+    Parameters
+    @results: the paht of output file.h5 storing training and test results
+    """
+
+    f = h5py.File(results, 'r')
+
+    data = f['classifier_accuracy_epoch_train']
+
+    if not os.path.exists(os.path.join(results.split('/')[0], 'Plots')): 
+        os.makedirs(os.path.join(results.split('/')[0], 'Plots'))
+    output_path = os.path.join(results.split('/')[0], 'Plots', 'accuracy_vs_epochs.png')
+
+    fig, ax = plt.subplots()
+    sample_points = [(i + 1) for i in range(len(data))]
+    ax.plot(sample_points, data, 'ro', sample_points, data, 'b')
+    ax.grid()
+    ax.set_xlabel('Epochs of training')
+    ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
     ax.set_ylabel('Accuracy')
     ax.set_title('Trends of Accuracy along Epochs')
 
-    plt.savefig('test.png')
+    plt.savefig(output_path)
 
-    # pdb.set_trace()
+def loss_vs_batches(results, interval): 
+    """
+    """
+    f = h5py.File(results, 'r')
+    data = f['classifier_loss_history_train']
+
+    if not os.path.exists(os.path.join(results.split('/')[0], 'Plots')): 
+        os.makedirs(os.path.join(results.split('/')[0], 'Plots'))
+    output_path = os.path.join(results.split('/')[0], 'Plots', 'loss_vs_batches.png')
+
+    fig, ax = plt.subplots()
+    sample_points = [(i + 1) * interval for i in range(len(data))]
+    # plt.style.use('ggplot')
+    ax.plot(sample_points, data, color='b')
+    ax.grid()
+    ax.set_xlabel('Batches of training')
+    ax.set_ylabel('Loss')
+    ax.set_title('Trends of Loss along Batches')
+
+    plt.savefig(output_path)
+
+def loss_vs_epochs(results): 
+    """
+    results = 'Output/Output008/results.h5'
+    Parameters
+    @results: the paht of output file.h5 storing training and test results
+    """
+
+    f = h5py.File(results, 'r')
+
+    data = f['classifier_loss_epoch_train']
+
+    if not os.path.exists(os.path.join(results.split('/')[0], 'Plots')): 
+        os.makedirs(os.path.join(results.split('/')[0], 'Plots'))
+    output_path = os.path.join(results.split('/')[0], 'Plots', 'loss_vs_epochs.png')
+
+    fig, ax = plt.subplots()
+    sample_points = [(i + 1) for i in range(len(data))]
+    ax.plot(sample_points, data, 'ro', sample_points, data, 'b')
+    ax.grid()
+    ax.set_xlabel('Epochs of training')
+    ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
+    ax.set_ylabel('Loss')
+    ax.set_title('Trends of Loss along Epochs')
+
+    plt.savefig(output_path)
 
 if __name__ == '__main__': 
-    f = h5py.File('../results.h5', 'r')
-    accuracy_vs_events(f, 20)
+    f = '../results.h5'
+    accuracy_vs_batches(f, 20)
+    accuracy_vs_epochs(f)
