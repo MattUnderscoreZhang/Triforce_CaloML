43a2d5c374dd36300c61432f1645776b0aeff0c1
justinliu412 <justinliu412@hotmail.com>
2018-03-30 19:27:38 -0500

    clean codes

--- test.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-
-options = {'outPath': os.getcwd()+"/Output/"+sys.argv[1]+"/"}
-
-import Tools.Classification_Plotter as Classification_Plotter
-
-Classification_Plotter.make_all(options['outPath']+'results.h5')
