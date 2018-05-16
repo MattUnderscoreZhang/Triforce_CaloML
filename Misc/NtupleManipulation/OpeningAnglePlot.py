21ae375488ccb1c9598c723e808bdafb1d6cb0d1
Matt Zhang <set.stun@gmail.com>
2018-05-16 00:16:21 -0500

    Corrected plots

--- Misc/NtupleManipulation/OpeningAnglePlot.py
+++ Misc/NtupleManipulation/OpeningAnglePlot.py
@@ -3,6 +3,7 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import seaborn as sns
 import glob
 import h5py as h5
 import numpy as np
@@ -25,30 +26,22 @@ for file_name in pi0_files:
     pi0_E += list(file['energy'][:])
     file.close()
 
-bins=np.arange(0,10,0.1)
-plt.hist(np.clip(pi0_opening_angle, bins[0], bins[-1]), bins=bins, density=True, histtype='step', label='Pi0')
-plt.title('Opening Angle of Pi0 Events')
-plt.xlabel('Opening Angle')
-plt.ylabel('Normalized Fraction')
-plt.savefig('opening_angles.png')
-
-plt.clf()
-
-bins=np.arange(0,1,0.01)
-plt.hist(np.clip(pi0_opening_angle, bins[0], bins[-1]), bins=bins, density=True, histtype='step', label='Pi0')
-plt.title('Opening Angle of Pi0 Events')
-plt.xlabel('Opening Angle')
+bins=np.arange(0,0.1,0.002)
+plt.hist(np.clip(pi0_opening_angle, bins[0], bins[-1]), bins=bins, density=True, histtype='step', linewidth=1, label='Pi0')
+plt.title('Opening Angle of Pi0 Events with Overflow Bin')
+plt.xlabel('Opening Angle (Radians)')
 plt.ylabel('Normalized Fraction')
 plt.savefig('zoom_opening_angles.png')
 
 plt.clf()
 
-bin_pi0_E_means = binned_statistic(pi0_opening_angle, pi0_E, bins=50, range=(0, 5)).statistic
+bin_pi0_E_means = binned_statistic(pi0_opening_angle, pi0_E, bins=20, range=(0, 0.2)).statistic
 
-plt.plot(np.arange(0,5,0.1), bin_pi0_E_means, label='Pi0')
+plt.plot(np.arange(0,0.2,0.01), bin_pi0_E_means, label='Pi0')
 plt.title('Mean Pi0 Energy in Opening Angle Bins')
-plt.xlabel('Opening Angle')
-plt.ylabel('Energy')
+plt.xlabel('Opening Angle (Radians)')
+plt.ylabel('Energy (GeV)')
+plt.grid()
 plt.savefig('mean_energy_vs_opening_angle.png')
 
 plt.clf()
@@ -56,8 +49,8 @@ plt.clf()
 pi0_pass_cut = [a < 0.01 for a in pi0_opening_angle]
 bin_pi0_pass_cut_means = binned_statistic(pi0_E, pi0_pass_cut, bins=50, range=(0, 500)).statistic
 
-plt.plot(np.arange(0,5,0.1), bin_pi0_pass_cut_means, label='Pi0')
+plt.plot(np.arange(0,500,10), bin_pi0_pass_cut_means, label='Pi0')
 plt.title('Fraction of Pi0 Events Passing Opening Angle < 0.01 Cut')
-plt.xlabel('Energy')
+plt.xlabel('Energy (GeV)')
 plt.ylabel('Fraction')
 plt.savefig('opening_angle_cut_vs_energy.png')
