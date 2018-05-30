006383765d7e2bb4e04c4fad97df80c72783893c
Matt Zhang <set.stun@gmail.com>
2018-05-29 19:32:32 -0500

    Added hyperparameters to BW submission scripts

--- Misc/BlueWatersSubmissionScripts/qsub.sh
+++ Misc/BlueWatersSubmissionScripts/qsub.sh
@@ -1,6 +1,8 @@
-hl=(4 5 6)
-ne=(256 512 1024)
-lr=(0.001 0.005 0.01)
+hl=(3, 4 5 6, 7)
+ne=(64 128 256 512 1024)
+lr=(0.0001 0.0005 0.001)
+dp=(0.2 0.3 0.4 0.5)
+ws=(11 21 31 41 51)
 
 for hl_i in ${hl[@]}
 do
@@ -8,9 +10,15 @@ do
     do
         for lr_i in ${lr[@]}
         do
-            sed 's/HYPERPARAMETERS_SEPARATED/'${hl_i}' '${ne_i}' '${lr_i}'/g' <qsub_template.in >qsub.in
-            sed 's/HYPERPARAMETERS/'${hl_i}_${ne_i}_${lr_i}'/g' -i qsub.in
-            qsub qsub.in
+            for dp_i in ${dp[@]}
+            do
+                for ws_i in ${ws[@]}
+                do
+                    sed 's/HYPERPARAMETERS_SEPARATED/'${hl_i}' '${ne_i}' '${lr_i}' '${dp_i}' '${ws_i}'/g' <qsub_template.in >qsub.in
+                    sed 's/HYPERPARAMETERS/'${hl_i}_${ne_i}_${lr_i}_${dp_i}_${ws_i}'/g' -i qsub.in
+                    qsub qsub.in
+                done
+            done
         done
     done
 done
