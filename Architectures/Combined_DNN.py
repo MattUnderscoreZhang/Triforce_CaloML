7b69e8c6dac1dee792c83e66c434c2e323cbb528
olivito <dominick.olivito@gmail.com>
2018-06-14 07:54:39 -0700

    use per-event weights for energy reg

--- Architectures/Combined_DNN.py
+++ Architectures/Combined_DNN.py
@@ -77,12 +77,20 @@ class Classifier_Net(nn.Module):
         return_data['classification'] = F.softmax(return_data['classification'].transpose(0, 1), dim=1)
         return return_data
 
+def weighted_mse_loss(pred,target,weights):
+    sqerr = (pred-target)**2
+    sqerr = sqerr * weights
+    loss = torch.mean(sqerr, dim=0)
+    return loss
+
 def lossFunction(output, data, term_weights):
     # classification loss: cross entropy
     loss_class = term_weights['classification'] * F.cross_entropy(output['classification'], Variable(data['pdgID'].cuda()))
     # regression loss: mse
-    # to add: per-event weights for energy regression
-    loss_energy = term_weights['energy_regression'] * F.mse_loss(output['energy_regression'], Variable(data['energy'].cuda()))
+    truth_energy = Variable(data['energy'].cuda())
+    # use per-event weights for energy to emphasize lower energies
+    event_weights = 1.0 / torch.log(truth_energy)
+    loss_energy = term_weights['energy_regression'] * weighted_mse_loss(output['energy_regression'], truth_energy, event_weights)
     loss_eta = term_weights['eta_regression'] * F.mse_loss(output['eta_regression'], Variable(data['eta'].cuda()))
     return loss_class+loss_energy+loss_eta
 
