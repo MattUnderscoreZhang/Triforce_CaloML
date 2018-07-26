86290e1f5d1090062e011d329d2821066a440bbd
Matt Zhang <set.stun@gmail.com>
2018-07-26 15:46:07 -0500

    Added random angle files to the bash script as well

--- Misc/SparseConversion/convert_all.sh
+++ Misc/SparseConversion/convert_all.sh
@@ -1,9 +1,17 @@
 declare -a infiles=("Ele" "Gamma" "Pi0")
-
 for folder_count in {1..8}; do
     for file_count in {1..10}; do
         for folder_name in $infiles; do
             python convert_sparse.py "/data/LCD/NewSamples/Fixed/"$folder_name"Escan_"$folder_count"_MERGED/"$folder_name"Escan_"$folder_count"_"$file_count".h5" "/data/LCD/NewSamples/Fixed/Sparse/"$folder_name"Escan_"$folder_count"_"$file_count".h5"
+        done
+    done
+done
+
+declare -a infiles=("Ele" "Gamma" "Pi0" "ChPi")
+for folder_count in {1..8}; do
+    for file_count in {1..10}; do
+        for folder_name in $infiles; do
+            python convert_sparse.py "/data/LCD/NewSamples/RandomAngle/"$folder_name"Escan_RandomAngle_MERGED/"$folder_name"Escan_"$folder_count"_"$file_count".h5" "/data/LCD/NewSamples/RandomAngle/Sparse/"$folder_name"Escan_"$folder_count"_"$file_count".h5"
         done
     done
 done
