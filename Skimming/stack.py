db71bf84f813200b781bb292fdb709b250b8e60c
Matt Zhang <set.stun@gmail.com>
2018-08-03 18:53:40 +0200

    Removed some unnecessary code

--- Skimming/stack.py
+++ Skimming/stack.py
@@ -13,7 +13,6 @@ output_path = sys.argv[2]
 events_per_output_file = sys.argv[3]
 
 output_file_counter = 0 # how many files we've written
-output_event_counter = 0 # how many events we've added to the output file
 output_data = {} # data to be written out
 
 for input_file in input_files:
@@ -31,18 +30,15 @@ for input_file in input_files:
         else:
             output_data[key] = np.concatenate(output_data[key], input_data)
 
-    # see how many events we've read in now
-    output_event_counter += input_file_n_events
-
     # if we have enough events, write output files
-    while output_event_counter >= events_per_output_file:
+    n_ready_events = output_data['ECAL'].shape[0]
+    while n_ready_events >= events_per_output_file:
         output_file = h5.File(output_path + "_" + str(output_file_counter) + ".h5" , 'w')
         for key in output_data.keys():
             output_file.create_dataset(key, data=output_data[key][:events_per_output_file])
             output_data[key] = output_data[key][events_per_output_file:]
         output_file.close()
         output_file_counter += 1
-        output_event_counter -= events_per_output_file
 
     # go to the next file
     input_file.close()
