85d55abbe853ed483646103be3714d3e390a6e96
Dawit Belayneh <dawitmesfin39@gmail.com>
2018-08-09 15:48:17 -0400

    added io test in studies.

---
+++ Misc/Studies/io_1.py
@@ -0,0 +1,170 @@
+## loader function from triforce.py isolated for testing ##
+
+################################################################
+#  ##########################################################  #
+#  #                                                        #  #
+#  #      /\\     TRIFORCE PARTICLE IDENTIFICATION SYSTEM   #  #
+#  #     /__\\    Classification, Energy Regression & GAN   #  #
+#  #    /\\ /\\                                             #  #
+#  #   /__\/__\\                       Run using Python 3   #  #
+#  #                                                        #  #
+#  ##########################################################  #
+################################################################
+
+import torch
+import torch.utils.data as data
+from torch.autograd import Variable
+import glob, os, sys, shutil, socket
+import numpy as np
+import h5py as h5
+import Loader.loader_multi as loader
+import Options
+sys.dont_write_bytecode = True # prevent the creation of .pyc files
+import pdb
+from timeit import default_timer as timer
+import resource as rs
+
+start = timer()
+
+# for running at caltech
+if 'culture-plate' in socket.gethostname():
+    import setGPU
+
+####################
+# Set options file #
+####################
+
+optionsFileName = "combined_caltech"
+
+######################################################
+# Import options & warn if options file has problems #
+######################################################
+
+# load options
+exec("from Options." + optionsFileName + " import *")
+
+# options file must have these parameters set
+requiredOptionNames = ['samplePath', 'classPdgID', 'trainRatio', 'nEpochs', 'batchSize', 'outPath']
+for optionName in requiredOptionNames:
+    if optionName not in options.keys():
+        print("ERROR: Please set", optionName, "in options file")
+        sys.exit()
+
+# if these parameters are not set, give them default values
+defaultParameters = {'importGPU':False, 'nTrainMax':-1, 'nValidationMax':-1, 'nTestMax':-1, 'validationRatio':0, 'nWorkers':0, 'calculate_loss_per_n_batches':20, 'test_loss_eval_max_n_batches':10, 'earlyStopping':False, 'relativeDeltaLossThreshold':0, 'relativeDeltaLossNumber':5, 'saveModelEveryNEpochs':0, 'saveFinalModel':0}
+for optionName in defaultParameters.keys():
+    if optionName not in options.keys():
+        options[optionName] = defaultParameters[optionName]
+
+# if validation parameters are not set, TriForce will use test set as validation set
+if options['validationRatio'] + options['trainRatio'] >= 1:
+    print("ERROR: validationRatio and/or trainRatio is too high")
+
+# warn if output directory already exists
+if not os.path.exists(options['outPath']):
+    os.makedirs(options['outPath'])
+else:
+    print("WARNING: Output directory already exists - overwrite (y/n)?")
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    while True:
+        choice = input().lower()
+        if choice in valid:
+            overwrite = valid[choice]
+            break
+        else:
+            print("Please respond with 'yes' or 'no'")
+    if overwrite:
+        shutil.rmtree(options['outPath'])
+        os.makedirs(options['outPath'])
+    else:
+        sys.exit()
+
+# copy code to output directory for logging purposes
+optionsFilePath = Options.__file__[:Options.__file__.rfind('/')]
+shutil.copyfile(optionsFilePath + "/" + optionsFileName + ".py", options['outPath']+"/options.py")
+shutil.copyfile(optionsFilePath + "/../triforce.py", options['outPath']+"/triforce.py")
+# also write options dict to store also command line input parameters
+with open(options['outPath']+"/options_dict.py", 'w') as f:
+    f.write('options = '+str(options))
+
+####################################
+# Load files and set up generators #
+####################################
+
+print('-------------------------------')
+print('Loading Files')
+
+# gather sample files for each type of particle
+nClasses = len(options['samplePath'])
+classFiles = [[]] * nClasses
+for i, classPath in enumerate(options['samplePath']):
+    classFiles[i] = glob.glob(classPath)
+
+# print(classFiles)
+# determine numbers of test, train, and validation files
+filesPerClass = min([len(files) for files in classFiles])
+nTrain = int(filesPerClass * options['trainRatio'])
+nValidation = int(filesPerClass * options['validationRatio'])
+nTest = filesPerClass - nTrain - nValidation
+if options['nTrainMax']>0:
+    nTrain = min(nTrain,options['nTrainMax'])
+if options['nValidationMax']>0:
+    nValidation = min(nValidation,options['nValidationMax'])
+if options['nTestMax']>0:
+    nTest = min(nTest,options['nTestMax'])
+if (options['validationRatio'] > 0):
+    print("Split (files per class): %d train, %d test, %d validation" % (nTrain, nTest, nValidation))
+else:
+    print("Split (files per class): %d train, %d test" % (nTrain, nTest))
+# if (nTest==0 or nTrain==0 or (options['validationRatio']>0 and nValidation==0)):
+#     print("Not enough files found - check sample paths")
+#     sys.exit()
+print('-------------------------------')
+
+# split the train, test, and validation files
+# get lists of [[class1_file1, class2_file1], [class1_file2, class2_file2], ...]
+trainFiles = []
+validationFiles = []
+testFiles = []
+for i in range(filesPerClass):
+    newFiles = []
+    for j in range(nClasses):
+        newFiles.append(classFiles[j][i]) 
+    if i < nTrain:
+        trainFiles.append(newFiles)
+    elif i < nTrain + nValidation:
+        validationFiles.append(newFiles)
+    elif i < nTrain + nValidation + nTest:
+        testFiles.append(newFiles)
+    else:
+        break
+if options['validationRatio'] == 0:
+    validationFiles = testFiles
+
+# print(trainFiles)    
+# prepare the generators
+# print(trainFiles)
+print('Defining training dataset')
+trainSet = loader.HDF5Dataset(trainFiles, options['classPdgID'], options['nWorkers'], options['nLoaders'], options['filters'])
+# print('Defining validation dataset')
+# validationSet = loader.HDF5Dataset(validationFiles, options['classPdgID'], options['filters'])
+# print('Defining test dataset')
+# testSet = loader.HDF5Dataset(testFiles, options['classPdgID'], options['filters'])
+trainLoader = data.DataLoader(dataset=trainSet,batch_size=options['batchSize'],sampler=loader.OrderedRandomSampler(trainSet),
+                             num_workers=options['nWorkers'], worker_init_fn=trainSet.init_worker)
+# validationLoader = data.DataLoader(dataset=validationSet,batch_size=options['batchSize'],sampler=loader.OrderedRandomSampler(validationSet))
+# testLoader = data.DataLoader(dataset=testSet,batch_size=options['batchSize'],sampler=loader.OrderedRandomSampler(testSet))
+
+end = timer()
+print('Total time taken to Setup files: %.2f seconds'%(float(end - start)))
+
+start = timer()
+for i, train_data in enumerate(trainLoader):
+    print("batch %d collected"%i)
+end = timer()
+
+time = end - start
+print("Total time to load 10,000 events = %.3f"%time)
+# outfile = open('output_compress1_split.txt','a+')
+# outfile.write("compress level - 1: %.3f"%time)
+# outfile.close()
