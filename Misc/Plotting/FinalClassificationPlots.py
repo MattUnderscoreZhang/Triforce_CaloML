9977b9d0b9f2abc8e94b8ad5aba13a0d3405d784
Matt Zhang <set.stun@gmail.com>
2018-11-26 17:04:29 -0600

    Made final classification plotting script easier to use

--- Misc/Plotting/FinalClassificationPlots.py
+++ Misc/Plotting/FinalClassificationPlots.py
@@ -1,7 +1,20 @@
+import matplotlib
+matplotlib.use('Agg')
 import h5py as h5
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import metrics
+
+##############
+# File Paths #
+##############
+
+base_path = "/home/mazhang/Triforce_CaloML/FinalResults/CLIC/GammaPi0/TriForce/"
+DNN_path = base_path + "Output_DNN_4_512_0.0002_0.04/"
+CNN_path = base_path + "Output_CNN_4_512_0.0004_0.12/"
+GN_path = base_path + "Output_GN_0_1024_0.0001_0.01/"
+BDT_path = "/home/mazhang/Triforce_CaloML/FinalResults/CLIC/GammaPi0/BDT/GammaPi0_WithR9/Results.h5"
+out_path = "Plots/GammaPi0/"
 
 ###################
 # Training Curves #
@@ -38,45 +51,41 @@ def plot_history(train, test, loss, batch, filename):
     plt.savefig(filename)
     plt.clf()
 
-# test_train_history = h5.File("/data/LCD/NewSamples/HyperparameterResults/DNN/DNN_Output_4_512_0.0002_0.04_Scan1/training_results.h5")
-test_train_history = h5.File("/data/LCD/NewSamples/HyperparameterResults/DNN/DNN_Output_4_512_0.0004_0.08_Scan1/training_results.h5")
-plot_history(test_train_history['class_acc_train_batch'], test_train_history['class_acc_test_batch'], loss=False, batch=True, filename="DNN_accuracy_batches.png")
+test_train_history = h5.File(DNN_path + "training_results.h5")
+plot_history(test_train_history['class_acc_train_batch'], test_train_history['class_acc_test_batch'], loss=False, batch=True, filename = out_path + "DNN_accuracy_batches.png")
 
-# test_train_history = h5.File("/data/LCD/NewSamples/HyperparameterResults/CNN/CNN_Output_4_512_0.0004_0.12_6_6_Scan1/training_results.h5")
-test_train_history = h5.File("/data/LCD/NewSamples/HyperparameterResults/CNN/CNN_Output_3_512_0.0004_0.08_6_6_Scan1/training_results.h5")
-plot_history(test_train_history['class_acc_train_batch'], test_train_history['class_acc_test_batch'], loss=False, batch=True, filename="CNN_accuracy_batches.png")
+test_train_history = h5.File(CNN_path + "training_results.h5")
+plot_history(test_train_history['class_acc_train_batch'], test_train_history['class_acc_test_batch'], loss=False, batch=True, filename = out_path + "CNN_accuracy_batches.png")
 
-test_train_history = h5.File("/data/LCD/NewSamples/HyperparameterResults/GN/GN_Output_1024_0.0001_0.01_Scan1/training_results.h5")
-plot_history(test_train_history['class_acc_train_batch'], test_train_history['class_acc_test_batch'], loss=False, batch=True, filename="GN_accuracy_batches.png")
+test_train_history = h5.File(GN_path + "training_results.h5")
+plot_history(test_train_history['class_acc_train_batch'], test_train_history['class_acc_test_batch'], loss=False, batch=True, filename = out_path + "GN_accuracy_batches.png")
 
 ##############
 # ROC Curves #
 ##############
 
-# final_val_results = h5.File("/data/LCD/NewSamples/HyperparameterResults/CNN/CNN_Output_4_512_0.0004_0.12_6_6_Scan1/validation_results.h5")
-final_val_results = h5.File("/data/LCD/NewSamples/HyperparameterResults/CNN/CNN_Output_3_512_0.0004_0.08_6_6_Scan1/validation_results.h5")
-truth = final_val_results['class_truth']
-scores = final_val_results['class_raw_prediction']
-fpr, tpr, thresholds = metrics.roc_curve(truth, scores)
-roc_auc = metrics.auc(fpr, tpr)
-plt.plot(fpr, tpr, color='green', lw=2, label='CNN (area = %0.2f)' % roc_auc)
-
-final_val_results = h5.File("/data/LCD/NewSamples/HyperparameterResults/GN/GN_Output_1024_0.0001_0.01_Scan1/validation_results.h5")
-truth = final_val_results['class_truth']
-scores = final_val_results['class_raw_prediction']
-fpr, tpr, thresholds = metrics.roc_curve(truth, scores)
-roc_auc = metrics.auc(fpr, tpr)
-plt.plot(fpr, tpr, color='red', lw=2, label='GN (area = %0.2f)' % roc_auc)
-
-# final_val_results = h5.File("/data/LCD/NewSamples/HyperparameterResults/DNN/DNN_Output_4_512_0.0002_0.04_Scan1/validation_results.h5")
-final_val_results = h5.File("/data/LCD/NewSamples/HyperparameterResults/DNN/DNN_Output_4_512_0.0004_0.08_Scan1/validation_results.h5")
+final_val_results = h5.File(DNN_path + "validation_results.h5")
 truth = final_val_results['class_truth']
 scores = final_val_results['class_raw_prediction']
 fpr, tpr, thresholds = metrics.roc_curve(truth, scores)
 roc_auc = metrics.auc(fpr, tpr)
 plt.plot(fpr, tpr, color='darkorange', lw=2, label='DNN (area = %0.2f)' % roc_auc)
 
-data = h5.File("/home/mazhang/Triforce_CaloML/Misc/BDT/Outputs/GammaPi0Results.h5")
+final_val_results = h5.File(CNN_path + "validation_results.h5")
+truth = final_val_results['class_truth']
+scores = final_val_results['class_raw_prediction']
+fpr, tpr, thresholds = metrics.roc_curve(truth, scores)
+roc_auc = metrics.auc(fpr, tpr)
+plt.plot(fpr, tpr, color='green', lw=2, label='CNN (area = %0.2f)' % roc_auc)
+
+final_val_results = h5.File(GN_path + "validation_results.h5")
+truth = final_val_results['class_truth']
+scores = final_val_results['class_raw_prediction']
+fpr, tpr, thresholds = metrics.roc_curve(truth, scores)
+roc_auc = metrics.auc(fpr, tpr)
+plt.plot(fpr, tpr, color='red', lw=2, label='GN (area = %0.2f)' % roc_auc)
+
+data = h5.File(BDT_path)
 fpr = data['fpr']
 tpr = data['tpr']
 roc_auc = metrics.auc(data['fpr'], data['tpr'])
@@ -90,4 +99,4 @@ plt.ylabel('True Positive Rate')
 plt.grid('on', linestyle='--')
 plt.title('ROC Curve for Classification')
 plt.legend(loc="lower right")
-plt.savefig("compare_ROC.png")
+plt.savefig(out_path + "compare_ROC.png")
