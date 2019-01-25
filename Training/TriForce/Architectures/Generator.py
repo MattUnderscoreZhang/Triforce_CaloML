f3a9db85f586ddcfbfde06a375e125af9c4ec35f
Matt Zhang <set.stun@gmail.com>
2019-01-24 23:18:54 -0600

    Brought Generator architecture up to date with PyTorch recommendation

--- Training/TriForce/Architectures/Generator.py
+++ Training/TriForce/Architectures/Generator.py
@@ -14,19 +14,19 @@ class Generator_Net(nn.Module):
         super().__init__()
         self.dense_1 = nn.Linear(1024, 64*7*7)
         self.conv_1 = nn.Conv3d(8, 64, (6,6,8), padding=(2,2,3))
-        init.uniform(self.conv_1.weight) 
+        init.uniform_(self.conv_1.weight) 
         self.batchnorm_1 = nn.BatchNorm3d(64)
         self.upsample_1 = nn.Upsample(scale_factor=2, mode='trilinear')
         
         self.conv_2 = nn.Conv3d(64, 6, (6,5,8))
-        init.uniform(self.conv_2.weight)
+        init.uniform_(self.conv_2.weight)
         self.batchnorm_2 = nn.BatchNorm3d(6)
         self.upsample_2 = nn.Upsample(scale_factor=(2,2,3), mode='trilinear')
 
         self.conv_3 = nn.Conv3d(6, 6 , (3,3,8))
-        init.uniform(self.conv_3.weight)
+        init.uniform_(self.conv_3.weight)
         self.conv_4 = nn.Conv3d(6, 1 , (2,2,2))
-        init.xavier_uniform(self.conv_4.weight)
+        init.xavier_uniform_(self.conv_4.weight)
 
 
     def forward(self, x):
