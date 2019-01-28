b383df2c3c8c421bbc9e7bc7ccd7051a4ba72a88
Matt Zhang <set.stun@gmail.com>
2019-01-28 16:00:46 -0600

    Repacking did Pi0 a second time instead of ChPi

--- SamplePrep/Resampling/Repacking/repack.sh
+++ SamplePrep/Resampling/Repacking/repack.sh
@@ -13,6 +13,6 @@ rm -rf Pi0 ChPi Gamma Ele
 mkdir Pi0 ChPi Gamma Ele
 
 CLIC_PATH="/public/data/calo/RandomAngle/CLIC"
-SAMPLE_PREFIXES=("Gamma/GammaEscan_RandomAngle" "Pi0/Pi0Escan_RandomAngle" "Ele/EleEscan_RandomAngle" "Pi0/Pi0Escan_RandomAngle")
+SAMPLE_PREFIXES=("Gamma/GammaEscan_RandomAngle" "Pi0/Pi0Escan_RandomAngle" "Ele/EleEscan_RandomAngle" "ChPi/ChPiEscan_RandomAngle")
 
 parallel source repack_one.sh ::: ${SAMPLE_PREFIXES} ::: {1..8} ::: {1..10} ::: $CLIC_PATH
