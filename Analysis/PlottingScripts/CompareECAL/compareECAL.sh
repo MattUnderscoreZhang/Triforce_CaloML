b81767f3f55077b805435ce81dc7edc6e8cfcf7e
Matt Zhang <set.stun@gmail.com>
2019-02-05 13:56:36 -0600

    Reorganization of plotting scripts

---
+++ Analysis/PlottingScripts/CompareECAL/compareECAL.sh
@@ -0,0 +1,6 @@
+python plotECAL.py /public/data/calo/FixedAngle/CLIC/Pi0/Pi0Escan_1_1.h5 CompareECAL/Fixed/CLIC/ 5
+python plotECAL.py /public/data/calo/FixedAngle/ATLAS/Pi0/Pi0Escan_1_1.h5 CompareECAL/Fixed/ATLAS/ 5
+python plotECAL.py /public/data/calo/FixedAngle/CMS/Pi0/Pi0Escan_1_1.h5 CompareECAL/Fixed/CMS/ 5
+python plotECAL.py /public/data/calo/RandomAngle/CLIC/Pi0/Pi0Escan_RandomAngle_1_1.h5 CompareECAL/Random/CLIC/ 5
+python plotECAL.py /public/data/calo/RandomAngle/ATLAS/Pi0/Pi0Escan_RandomAngle_1_1.h5 CompareECAL/Random/ATLAS/ 5
+python plotECAL.py /public/data/calo/RandomAngle/CMS/Pi0/Pi0Escan_RandomAngle_1_1.h5 CompareECAL/Random/CMS/ 5
