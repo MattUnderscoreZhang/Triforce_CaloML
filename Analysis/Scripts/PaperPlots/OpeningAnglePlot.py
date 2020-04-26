2a6ceb86bdd4e2c39a9f3a9f345205104bd5c5d9
Matt Zhang <set.stun@gmail.com>
2020-04-26 14:54:53 -0500

    Reformatted opening angle plots

--- Analysis/Scripts/PaperPlots/OpeningAnglePlot.py
+++ Analysis/Scripts/PaperPlots/OpeningAnglePlot.py
@@ -1,7 +1,7 @@
 import matplotlib
 matplotlib.use('Agg')  # NOQA
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import glob
 import h5py as h5
 import numpy as np
@@ -10,7 +10,7 @@ from scipy.stats import binned_statistic
 """Figuring out what the best opening angle cut should be for Gamma and Pi0."""
 
 # pi0_path = "/u/sciteam/zhang10/Projects/DNNCalorimeter/Data/NewSamples/Fixed/Pi0Escan_*_MERGED/Pi0Escan_*.h5"
-pi0_path = "/public/data/calo/RandomAngle/CLIC/Pi0/Pi0Escan*.h5"
+pi0_path = "/public/data/Calo/RandomAngle/CLIC/Pi0/Pi0Escan*.h5"
 
 ###########
 # COMBINE #
@@ -31,9 +31,10 @@ for file_name in pi0_files:
 
 bins = np.arange(0, 0.1, 0.002)
 plt.hist(np.clip(pi0_opening_angle, bins[0], bins[-1]), bins=bins, density=True, histtype='step', linewidth=1, label='Pi0')
-plt.title('Opening Angle of Pi0 Events with Overflow Bin', fontsize=15)
+# plt.title('Opening Angle of Pi0 Events with Overflow Bin', fontsize=15)
 plt.xlabel('Opening Angle (Radians)', fontsize=12)
 plt.ylabel('Normalized Fraction', fontsize=12)
+plt.yscale('log')
 plt.savefig('zoom_opening_angles.png')
 
 plt.clf()
@@ -41,7 +42,7 @@ plt.clf()
 bin_pi0_E_means = binned_statistic(pi0_opening_angle, pi0_E, bins=20, range=(0, 0.2)).statistic
 
 plt.plot(np.arange(0, 0.2, 0.01), bin_pi0_E_means, label='Pi0')
-plt.title('Mean Pi0 Energy in Opening Angle Bins', fontsize=15)
+# plt.title('Mean Pi0 Energy in Opening Angle Bins', fontsize=15)
 plt.xlabel('Opening Angle (Radians)', fontsize=12)
 plt.ylabel('Energy (GeV)', fontsize=12)
 # plt.grid()
@@ -53,7 +54,7 @@ pi0_pass_cut = [a < 0.01 for a in pi0_opening_angle]
 bin_pi0_pass_cut_means = binned_statistic(pi0_E, pi0_pass_cut, bins=50, range=(0, 500)).statistic
 
 plt.plot(np.arange(0, 500, 10), bin_pi0_pass_cut_means, label='Pi0')
-plt.title('Fraction of Pi0 Events Passing Opening Angle < 0.01 Cut', fontsize=15)
+# plt.title('Fraction of Pi0 Events Passing Opening Angle < 0.01 Cut', fontsize=15)
 plt.xlabel('Energy (GeV)', fontsize=12)
 plt.ylabel('Fraction', fontsize=12)
 plt.savefig('opening_angle_cut_vs_energy.png')
