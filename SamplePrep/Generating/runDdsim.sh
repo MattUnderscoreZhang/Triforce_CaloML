fa0c1cd661ffa69c8cdd797840475893b6b0eaad
Matt Zhang <set.stun@gmail.com>
2022-05-18 17:37:57 -0400

    Make runDdsim.sh create only one particle

--- SamplePrep/Generating/runDdsim.sh
+++ SamplePrep/Generating/runDdsim.sh
@@ -3,10 +3,14 @@ source /cvmfs/clicdp.cern.ch/iLCSoft/builds/2016-11-22/x86_64-slc6-gcc48-opt/ini
 # generate using input generator file
 #ddsim --compactFile /cvmfs/clicdp.cern.ch/iLCSoft/builds/2016-11-22/x86_64-slc6-gcc48-opt/lcgeo/HEAD/CLIC/compact/CLIC_o3_v07/CLIC_o3_v07.xml -O ROOTFiles/eejj.root -N 1000 --random.seed 6546777 --inputFiles GeneratorFiles/eejj.hepevt 
 
+# create output folder
+mkdir ROOTFiles
+
 # particle gun input
-for energy in `seq 50 10 100`
+for energy in `seq 50 10 50`
 do
-    for fileN in {1..10}
+    #for fileN in {1..10}
+    for fileN in {1..1}
     do
         ddsim --compactFile /cvmfs/clicdp.cern.ch/iLCSoft/builds/2016-11-22/x86_64-slc6-gcc48-opt/lcgeo/HEAD/CLIC/compact/CLIC_o3_v07/CLIC_o3_v07.xml -O ROOTFiles/pi0_${energy}_GeV_${fileN}.root -N 1000 --enableGun --gun.particle pi0 --gun.energy $energy*GeV --gun.distribution uniform --gun.direction 1,0,0
         ddsim --compactFile /cvmfs/clicdp.cern.ch/iLCSoft/builds/2016-11-22/x86_64-slc6-gcc48-opt/lcgeo/HEAD/CLIC/compact/CLIC_o3_v07/CLIC_o3_v07.xml -O ROOTFiles/gamma_${energy}_GeV_${fileN}.root -N 1000 --enableGun --gun.particle gamma --gun.energy $energy*GeV --gun.distribution uniform --gun.direction 1,0,0
