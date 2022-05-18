03d48233b51cc88d9875878a1237a1f118e5f0bf
Matt Zhang <set.stun@gmail.com>
2022-05-18 17:55:27 -0400

    Updated skimming script

--- SamplePrep/Filtering/skimH5.py
+++ SamplePrep/Filtering/skimH5.py
@@ -1,39 +1,53 @@
 import h5py
 import h5pp
 import numpy as np
+from os import listdir
+from os.path import isfile, join
+import sys, getopt
 
-oldFilePath = "../AllFiles/H5Files/v1/Unskimmed_withOpeningAngle_FixedCoordinates/"
-newFilePath = "../AllFiles/H5Files/v1/Skimmed_withOpeningAngle_FixedCoordinates/"
 
-filesToSkim = []
+helpline = 'skimH5.py -i <inputPath> -o <outputPath>'
 
-# for fileN in range(1, 11):
-for fileN in range(1, 2):
-    # filesToSkim.append("gamma_60_GeV_"+str(fileN)+".h5")
-    filesToSkim.append("pi0_60_GeV_"+str(fileN)+".h5")
 
-for fileName in filesToSkim:
+def main(argv):
+    try:
+        opts, _ = getopt.getopt(argv, "hi:o:", ["inputPath=", "outputPath="])
+    except getopt.GetoptError:
+        print helpline
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print helpline
+            sys.exit()
+        elif opt in ("-i", "--inputPath"):
+            inputPath = arg
+        elif opt in ("-o", "--outputPath"):
+            outputPath = arg
+        print 'Converting all files found in ', inputPath
+        print 'Saving converted files in ', outputPath
 
-    print "Skimming", fileName
+        # loop over all the files in the inputPath folder
+        for fileName in [file for file in listdir(inputPath) if isfile(join(inputPath, file))]:
+            print "Skimming", fileName
 
-    oldFile = h5py.File(oldFilePath + fileName)
-    newFile = h5py.File(newFilePath + fileName, "w")
+            oldFile = h5py.File(inputPath + fileName)
+            newFile = h5py.File(outputPath + fileName, "w")
 
-    badIndices = [index for index, ratio in enumerate(oldFile['HCAL_ECAL_Ratios/HCAL_ECAL_ERatio']) if ratio > 0.025 or not np.isfinite(ratio)]
+            badIndices = [index for index, ratio in enumerate(oldFile['HCAL_ECAL_Ratios/HCAL_ECAL_ERatio']) if ratio > 0.025 or not np.isfinite(ratio)]
 
-    # recursively list all datasets in the sample
-    datasets = []
+            # recursively list all datasets in the sample
+            datasets = []
 
-    def appendName(name):
-        if isinstance(oldFile[name], h5py.Dataset):
-            datasets.append(name)
-        return None
+            def appendName(name):
+                if isinstance(oldFile[name], h5py.Dataset):
+                    datasets.append(name)
+                return None
 
-    oldFile.visit(appendName)
+            oldFile.visit(appendName)
 
-    if len(badIndices) < len(oldFile['HCAL_ECAL_Ratios/HCAL_ECAL_ERatio']):
-        for dsetName in datasets:
-            dset = h5pp.deleteRows(oldFile, dsetName, badIndices, newFile)
+            if len(badIndices) < len(oldFile['HCAL_ECAL_Ratios/HCAL_ECAL_ERatio']):
+                for dsetName in datasets:
+                    dset = h5pp.deleteRows(oldFile, dsetName, badIndices, newFile)
 
-    oldFile.close()
-    newFile.close()
+            oldFile.close()
+            newFile.close()
